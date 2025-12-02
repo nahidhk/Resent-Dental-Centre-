e344f329c54a62c8547c442c067dae3289b4751b
nahidhk <nahidappltd@gmail.com>
2025-12-03 01:39:59 +0600

    update css

--- src/components/Err/ErrApi.jsx
+++ src/components/Err/ErrApi.jsx
@@ -1,37 +1,38 @@
 import { useEffect, useState } from "react";
 import api from "../../api/api.json";
 
-
 export default function ErrApi() {
-    
 
-  const [offline, setOffline] = useState(false);
-
-  const apiurl = `${api.request}://${api.server}`;
+  const [apiError, setError] = useState();
+  const apiUrl = `${api.request}://${api.server}/online.json`
 
   useEffect(() => {
-    fetch(apiurl)
-      .then(res => {
-        if (!res.ok) throw new Error("API Down");
-        setOffline(false);
-      })
-      .catch(err => {
-        setOffline(true);
-      });
-  }, []);  
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => setError(data))
 
+  });
+
+  console.log(apiError);
 
   return (
     <>
-      {offline && (
+      {/* {apiError ? (
         <div className="bottom w100 index flex center medel padding err left">
-          Error fetching api data, Page reload and try again!
-          This is a MySQL API Problem<br />
+          Error fetching API data!<br />
+          Page reload and try again! <br />
+          This is a MySQL API Problem <br />
           API KEY: {api.apikey} <br />
           API Server: {api.server} <br />
-          Need Help the call Developer Support: +88 0963 857 3063 
+          Need Help? Call Developer Support: +88 0963 857 3063
         </div>
-      )}
+      )
+        :
+        ""
+      } */}
+
+
+
     </>
   );
 }
