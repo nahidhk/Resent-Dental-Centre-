c1f990b5bc90748edc31468f52c58fa39d4458d6
Nahid HK <nahidappltd@gmail.com>
2025-12-04 15:48:40 +0600

    Update cc oe

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home';
 import Insert from './pages/Insert';
 import Toast from './components/system/Toast';
-import Login from './components/login/Login';
+//import Login from './components/login/Login';
 
 //console
 import eruda from "eruda";
@@ -34,7 +34,7 @@ function App() {
             </Routes>
           </div>
           <Toast />
-          <Login />
+          
         </div>
       </>
     )
--- src/components/prepction/CcOeADVX_ray.jsx
+++ src/components/prepction/CcOeADVX_ray.jsx
@@ -10,6 +10,23 @@ export default function CcOeADVX_ray() {
   const handleCcGraps = (e) => {
     setccgraps(e.target.checked);
   }
+  //#info- C/C  value
+   // cc text input
+  const [ccNotesData, setCcNotesData] = useState("");
+   //cc graps 
+   const [ccRT, setCcRT] = useState(false);
+   const [ccLT, setCcLT] = useState(false);
+   const [ccRB, setCcRB] = useState(false);
+   const [ccLB, setCcLB] = useState(false);
+   
+   const cCGraps = {
+     ccRT:ccRT,
+     ccLT:ccLT,
+     ccRB:ccRB,
+     ccLB:ccLB
+   }
+   console.log(cCGraps);
+   
 
   return (
     <>
@@ -31,7 +48,7 @@ export default function CcOeADVX_ray() {
               </p>
               {
                 ccnotes === true ? (
-                  <input className="input animation" type="text" />
+                  <input  className="input animation" type="text" />
                 ) : ("")
               }
             </div>
@@ -50,23 +67,28 @@ export default function CcOeADVX_ray() {
                   (
                     <div className="flex cloman animation">
                       <div className="flex">
+                    
                         <div className="rayBox borderRB">
-                          <input type="checkbox" className="checkSystem" />
+                          <input type="checkbox" className="checkSystem" 
+                        onChange={(e) => setCcLT(e.target.checked)}  
+                          />
                         </div>
                         <div className="rayBox borderLB">
-                          <input type="checkbox" className="checkSystem" />
+                          <input type="checkbox" className="checkSystem" 
+                   onChange={(e) => setCcRT(e.target.checked)}       
+                          />
                         </div>
                       </div>
                       <div className="flex">
                         <div className="rayBox borderRT">
-                          <input type="checkbox" className="checkSystem" />
+                          <input type="checkbox" className="checkSystem" onChange={(e) => setCcLB(e.target.checked)}/>
                         </div>
                         <div className="rayBox borderLT">
-                          <input type="checkbox" className="checkSystem" />
+                          <input type="checkbox" className="checkSystem"  
+                          onChange={(e) => setCcRB(e.target.checked)}/>
                         </div>
                       </div>
                     </div>
-
                   )
                   :
                   ("")
@@ -74,6 +96,29 @@ export default function CcOeADVX_ray() {
             </div>
           </div>
         </fieldset>
+        
+        {/*OE*/}
+        <fieldset>
+          <legend>
+            <span className="bigText">
+            O/E  
+            </span>
+          </legend>
+          <div className="border padding opction">
+            <p className="flex medel">Eneble Notes Opction
+              <input type="checkbox" />
+            </p>
+          </div>
+          
+          <div className="border padding opction">
+            <p className="flex medel">
+                Enable Graps opction
+           <input type="checkbox" />
+         
+            </p>
+          </div>
+        </fieldset>
+        
       </div>
     </>
   );
