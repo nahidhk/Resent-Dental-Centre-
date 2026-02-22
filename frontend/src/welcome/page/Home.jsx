d3aa8d6a3451cdfd9d5ad83180e0bf35008317db
NAHID HK <nahidappltd@gmail.com>
2026-02-23 01:45:40 +0600

    new update

--- frontend/src/welcome/page/Home.jsx
+++ frontend/src/welcome/page/Home.jsx
@@ -9,12 +9,51 @@ export default function Home() {
                 <div className="flex beet medel">
                     <div>
                         <div className="web-name">
-                            Your Perfect Smile Starts with Resent Dental
+                            <span className="vw5">
+                                Your Perfect Smile Starts with Resent Dental
+                            </span>
+                            <p className="smailTitle">
+                                Advanced dental care with a gentle touch. <br />
+                                Book your appointment today.
+                            </p>
+                            <br /><br />
+                            <div className="flex center beet">
+                                <div className="web-card">
+                                    <div className="workTimeBox flex center medel ">
+                                        <div>
+                                            <h2>
+                                                Working Time
+                                            </h2>
+                                            <br />
+                                            <p>
+                                                Monday-Friday 
+                                                9AM-9PM <br />
+                                                Saturday, Sunday 
+                                                10AM-6PM
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                       <div className="tofay flex medel center">
+                                        today
+                                       </div>
+                                    </div>
+                                </div>
+
+                                <div className="web-card flex center medel clomanC pinBtn">
+                                    <span>
+                                        Icon
+                                    </span>
+                                    <p>
+                                        This is smaill text
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
 
-                    <div className="flex center medel">
+                    <div className="flex center medel imgBox">
                         <img src={HomeImg} alt="" className="web-homeImg" />
                     </div>
                 </div>
